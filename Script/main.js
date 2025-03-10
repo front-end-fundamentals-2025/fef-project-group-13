2b326bebe1df3b671569d6e963f23d0fe865c6b4
LopasReng <galo24bj@student.ju.se>
2025-03-10 10:50:21 +0100

    Continuation of shopping cart logic

--- Script/main.js
+++ Script/main.js
@@ -6,3 +6,18 @@ if (sessionStorage.getItem("total") == null) {
   let amount = Number(sessionStorage.getItem("total"));
   document.getElementById("amount").innerHTML = amount;
 }
+
+function addToCart(id){
+    if (sessionStorage.getItem("amount" + id.toString())== null){
+        sessionStorage.setItem("amount" + id.toString(), "1");
+
+        sessionStorage.setItem(
+            id.toString(),
+            document.getElementById(id).outerHTML
+        );
+    } else {
+        let amount = Number(sessionStorage.getItem("amount"+ id.toString()))
+        amount++;
+        sessionStorage.setItem("amount" + id.toString(), amount.toString());
+    }
+}
