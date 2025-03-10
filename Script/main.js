125ddfeee9f63d432dcf1dfd157239b077c05625
BenjiDaHacker <gube24bx@student.ju.se>
2025-03-10 10:56:16 +0100

    shopping cart logic

--- Script/main.js
+++ Script/main.js
@@ -7,17 +7,32 @@ if (sessionStorage.getItem("total") == null) {
   document.getElementById("amount").innerHTML = amount;
 }
 
-function addToCart(id){
-    if (sessionStorage.getItem("amount" + id.toString())== null){
-        sessionStorage.setItem("amount" + id.toString(), "1");
+function addToCart(id) {
+  if (sessionStorage.getItem("amount" + id.toString()) == null) {
+    sessionStorage.setItem("amount" + id.toString(), "1");
 
-        sessionStorage.setItem(
-            id.toString(),
-            document.getElementById(id).outerHTML
-        );
-    } else {
-        let amount = Number(sessionStorage.getItem("amount"+ id.toString()))
-        amount++;
-        sessionStorage.setItem("amount" + id.toString(), amount.toString());
-    }
+    sessionStorage.setItem(
+      id.toString(),
+      document.getElementById(id).outerHTML
+    );
+  } else {
+    let amount = Number(sessionStorage.getItem("amount" + id.toString()));
+    amount++;
+    sessionStorage.setItem("amount" + id.toString(), amount.toString());
+  }
+
+  let amount = Number(sessionStorage.getItem("total"));
+  amount++;
+  sessionStorage.setItem("total", amount.toString());
+  document.getElementById("amount").innerHTML = amount;
+}
+
+function showCart() {
+  document.getElementById("main").innerHTML = sessionStorage.getItem("2");
+}
+
+function removeFromCart(id) {
+  sessionStorage.removeItem(id.toString());
+  sessionStorage.removeItem("amount" + id.toString());
+  document.getElementById("main").innerHTML = "";
 }
