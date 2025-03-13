0d18b216303a0980ed6c0e88aef1a4f514e82f77
BenjiDaHacker <gube24bx@student.ju.se>
2025-03-13 15:35:18 +0100

    shopping cart logic

--- Script/main.js
+++ Script/main.js
@@ -1,42 +1,16 @@
-const empty = document.createElement("div");
-// The storage is 0 when you open the website
-if (sessionStorage.getItem("total") == null) {
-  sessionStorage.setItem("total", "0");
-} else {
-  let amount = Number(sessionStorage.getItem("total"));
-  document.getElementById("amount").innerHTML = amount;
+let prodlist = [];
+
+if (sessionStorage.getItem("products") != null) {
+  prodlist = JSON.parse(sessionStorage.getItem("products"));
 }
 
-// Store the id-element in the cart (product)
-function addToCart(id) {
-  if (sessionStorage.getItem("amount" + id.toString()) == null) {
-    sessionStorage.setItem("amount" + id.toString(), "1");
-
-    sessionStorage.setItem(
-      id.toString(),
-      document.getElementById(id).outerHTML
-    );
-  // Stores multiple id's in cart and sessionStorage
-  } else {
-    let amount = Number(sessionStorage.getItem("amount" + id.toString()));
-    amount++;
-    sessionStorage.setItem("amount" + id.toString(), amount.toString());
-  }
-
-  let amount = Number(sessionStorage.getItem("total"));
-  amount++;
-  sessionStorage.setItem("total", amount.toString());
-  document.getElementById("amount").innerHTML = amount;
+function cartAdder(id) {
+  prodlist.push(id);
+  sessionStorage.setItem("products", JSON.stringify(prodlist));
+  cart = document.getElementById("cart");
+  cart.innerHTML = prodlist.length;
 }
 
-// Sows stored products in the checkout page
-function showCart() {
-  document.getElementById("main").innerHTML = sessionStorage.getItem("2");
-}
-
-// Removes stored items from checkout page and sessionStorage
-function removeFromCart(id) {
-  sessionStorage.removeItem(id.toString());
-  sessionStorage.removeItem("amount" + id.toString());
-  document.getElementById("main").innerHTML = "";
-}
+document.getElementById("cartbutton").addEventListener("click", function () {
+  cartAdder(1);
+});
